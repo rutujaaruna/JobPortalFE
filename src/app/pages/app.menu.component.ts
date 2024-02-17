ca86241f5b00fc3d4d18d3b4d7cbf926e0f7da39
Rutuja Patil <rutujapatil978@gmail.com>
2024-02-17 21:02:36 +0530

    Revert "Reapply "RP-Feat- added authentication to route.""
    This reverts commit cfb8be5bd7531893a3a77861c8a7f3be87c59dd2.

--- src/app/pages/app.menu.component.ts
+++ src/app/pages/app.menu.component.ts
@@ -1,75 +1,32 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { LayoutService } from '../services/app.layout.service';
-import { AuthenticationService } from '../services/authentication.service';
-import { of } from 'rxjs';
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './app.menu.component.html',
+    selector: 'app-menu',
+    templateUrl: './app.menu.component.html'
 })
 export class AppMenuComponent implements OnInit {
-  model: any[] = [];
-  userRole!: string;
 
-  constructor(
-    public layoutService: LayoutService,
-    private authService: AuthenticationService
-  ) {}
+    model: any[] = [];
 
-  ngOnInit() {
-    this.checkLoginStatus();
-    this.model = [
-      {
-        label: '',
-        items: [
-          {
-            label: 'Dashboard',
-            icon: 'pi pi-fw pi-home',
-            routerLink: ['/pages/dashboard'],
-          },
-          {
-            label: 'Job-List',
-            icon: 'pi pi-fw pi-briefcase',
-            routerLink: ['/pages/job-list'],
-          },
-          {
-            label: 'Applied Job',
-            icon: 'pi pi-fw pi-user',
-            routerLink: ['/pages/applied-job'],
-          },
-          {
-            label: 'Saved Job',
-            icon: 'pi pi-fw pi-users',
-            routerLink: ['/pages/saved-job'],
-          },
-          {
-            label: 'Profile',
-            icon: 'pi pi-fw pi-book',
-            routerLink: ['/pages/profile'],
-          },
-          {
-            label: 'About-Us',
-            icon: 'pi pi-fw pi-calendar-plus',
-            routerLink: ['/pages/about-us'],
-          },
-          {
-            label: 'Post Job',
-            icon: 'pi pi-fw pi-calendar-plus',
-            routerLink: ['/pages/recruiter/add-job'],
-          },
-        ],
-      },
-    ];
-  }
+    constructor(public layoutService: LayoutService) { }
 
-  checkLoginStatus() {
-    const user = this.authService.isLoggedIn();
-    if (user && user.id && user.id.role) {
-      this.userRole = user.id.role;
-      console.log(this.userRole); // Do something with the user data
-    } else {
-      // Handle scenario when user or user id or user role is not available
+    ngOnInit() {
+      localStorage.getItem('token')
+        this.model = [
+          {
+            label: '', 
+            items: [
+                { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/pages/dashboard'] },
+                { label: 'Job-List', icon: 'pi pi-fw pi-briefcase', routerLink: ['/pages/job-list'] },
+                { label: 'Applied Job', icon: 'pi pi-fw pi-user', routerLink: ['/pages/applied-job'] },
+                { label: 'Saved Job', icon: 'pi pi-fw pi-users', routerLink: ['/pages/saved-job'] },
+                { label: 'Profile', icon: 'pi pi-fw pi-book', routerLink: ['/pages/profile'] },
+                { label: 'About-Us', icon: 'pi pi-fw pi-calendar-plus', routerLink: ['/pages/about-us'] },
+                { label: 'Post Job', icon: 'pi pi-fw pi-calendar-plus', routerLink: ['/pages/recruiter/add-job'] }
+            ]
+          }
+        ];
     }
-  }
 }
